8f2aae4ff276f7be673eec073b27954c48e90d10
Fran Alvarez <fjalvarez@gmail.com>
2018-03-16 18:18:14 +0100

    Remove emojis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,4 @@ import { Component } from '@angular/core';
     styleUrls: ["app.component.css"]
 })
 export class AppComponent {
-    emoji = ['🎉', '😍', '😜', '👍'];
-    activeEmoji: string;
-
-    changeEmoji() {
-        this.activeEmoji = this.emoji[Math.floor(Math.random() * this.emoji.length)];
-    }
 }
